4fa62e3a4c95e6f57722355c90eec37a204f21c0
MOUFADIL walid <walid.moufadil-etu@etu.univh2c.ma>
2026-01-02 16:00:30 +0100

    update service

---
+++ src/test/java/org/example/cqrsaxon/CqrsAxonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cqrsaxon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqrsAxonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
